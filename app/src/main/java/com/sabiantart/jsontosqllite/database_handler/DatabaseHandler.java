a8434e9ed399148c69f3866a710998224782eb36
sabiduria <antdepadoue@gmail.com>
2024-07-11 19:30:25 +0200

    DatabaseHandler Class

---
+++ app/src/main/java/com/sabiantart/jsontosqllite/database_handler/DatabaseHandler.java
@@ -0,0 +1,30 @@
+package com.sabiantart.jsontosqllite.database_handler;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler  extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    protected static final String DATABASE_NAME = "json2sqlite";
+    private static final String TABLE_USERS = "users";
+    private static final String CREATE_TABLE_USERS = "CREATE TABLE " + TABLE_USERS +" (id TEXT, idrole TEXT, username TEXT, password TEXT, email TEXT, avatar TEXT, prenom TEXT, nom TEXT, telephone TEXT, statut TEXT, dateinscription TEXT, iduser TEXT, datemiseajour TEXT, sexe TEXT, codeuser TEXT, rolead1 TEXT, rolead2 TEXT, rolead3 TEXT, section TEXT, langue TEXT, synced TEXT);";
+
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_USERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_USERS);
+    }
+}
